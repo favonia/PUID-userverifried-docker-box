2d4cf84acd8eb85aec6909f0934653063cd50f2a
userverifried <userverifried@gmail.com>
2023-05-29 16:23:04 +1000

    Error debugged \& fixed

--- caddy/README.md
+++
@@ -1,4 +0,0 @@
-# vaultwarden-caddy-config
-Vaultwarden-Caddy Docker Compose
-
-Docker Compose and Caddyfile required to self-host vaultwarden with SSL
--- install-kasm.sh
+++ install-kasm.sh
@@ -3,4 +3,4 @@
 cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.13.1.421524.tar.gz
 tar -xf kasm_release_1.13.1.421524.tar.gz
-sudo bash kasm_release/install.sh --accept-eula --swap-size 35536  --proxy-port 8443 --use-rolling-images --admin-password This1s@adminpw# --user-password This1s@userpw#
+sudo bash kasm_release/install.sh --accept-eula --swap-size 35536  --proxy-port 9443 --admin-password This1s@adminpw# --user-password This1s@userpw#
